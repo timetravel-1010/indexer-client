f0e5fe4406977e81b1d6607547cd8bf848ab277b
timetravel-1010 <timetravel_1010@protonmail.com>
2024-01-30 21:51:37 -0500

    Add Hit and EmailResponse models

--- src/components/EmailInbox.vue
+++ src/components/EmailInbox.vue
@@ -3,53 +3,53 @@ import { ref } from 'vue';
 import type { Email } from '../models/Email';
 import InboxTable from './InboxTable.vue';
 import ContentPane from './ContentPane.vue';
+import type { EmailResponse } from '../models/EmailResponse';
+import type { Hit } from '../models/Hit';
 
-const emails = ref<Email[]>([]);
+const emails = ref<Hit[]>([]);
 const contentPane = ref();
 
 const onDisplayEmail = (e: Email) => {
-  console.log("row clicled!");
-  contentPane.value.displayEmail(e);
+    console.log("row clicled!");
+    contentPane.value.displayEmail(e);
 }
 
 const searchEmails = (term: string) => {
-  console.log("term:", term);
-  let url = new URL('http://localhost:8080/email/search?index=foo2024&page=10');
-  url.searchParams.append("term", term);
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
+    console.log("term:", term);
+    let url = new URL('http://localhost:8080/email/search?index=foo2024&page=10');
+    url.searchParams.append("term", term);
+    fetch(url, {
+        method: 'GET',
     })
-    .then((data: Email[]) => {
-      emails.value = data
-      console.log(emails.value)
-    })
-    .catch(error => {
-      console.error('Error: ', error);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data: EmailResponse) => {
+            emails.value = data.hits.hits;
+            console.log('data: ', data.hits);
+            console.log('res:', emails.value)
+        })
+        .catch(error => {
+            console.error('Error: ', error);
+        });
 };
 
 defineExpose({
-  searchEmails
+    searchEmails
 });
 
 </script>
 
 
 <template>
-  <div class="flex flex-col w-1/2">
-    <InboxTable @on-select="onDisplayEmail" :emails=emails />
-    <button>Load more</button>
-  </div>
-  <ContentPane ref="contentPane" />
+    <div class="flex flex-col w-1/2">
+        <InboxTable @on-select="onDisplayEmail" :emails=emails />
+        <button>Load more</button>
+    </div>
+    <ContentPane ref="contentPane" />
 </template>
 
 
--- src/models/Email.ts
+++ src/models/Email.ts
@@ -1,25 +1,20 @@
 export interface Email {
-    _id: string;
-    _source: {
-        email: {
-            messageId: string;
-            date: string;
-            from: string;
-            to: string[];
-            cc: string[];
-            bcc: string[];
-            subject: string;
-            mimeVersion: string;
-            contentType: string;
-            contentTransferEncoding: string;
-            xFrom: string;
-            xTo: string[];
-            xcc: string[];
-            xbcc: string[];
-            xFolder: string;
-            xOrigin: string;
-            xFileName: string;
-            body: string;
-        };
-    };
+    messageId: string;
+    date: string;
+    from: string;
+    to: string[];
+    cc: string[];
+    bcc: string[];
+    subject: string;
+    mimeVersion: string;
+    contentType: string;
+    contentTransferEncoding: string;
+    xFrom: string;
+    xTo: string[];
+    xcc: string[];
+    xbcc: string[];
+    xFolder: string;
+    xOrigin: string;
+    xFileName: string;
+    body: string;
 }
